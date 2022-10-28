7bb0d5b06dfc6104952c8ccd555f8a80b30aed75
email2ify <email2ify@yahoo.com>
2022-10-28 11:46:19 +0000

    Set up structure of Game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnwer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function calculateWrongAnwer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function diplayMultiplyQuestion(){
+    
+}
