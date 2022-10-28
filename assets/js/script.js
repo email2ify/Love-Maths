4e042ec2943cec86f2fa91970ca79ec56e11c5ae
email2ify <email2ify@yahoo.com>
2022-10-28 18:45:10 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,15 @@ document.addEventListener("DOMContentLoaded", function(){
       for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-            alert("You clicked Submit!");
+            //alert("You clicked Submit!");
+            checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 //alert(`You clicked ${gameType}`);
                 runGame(gameType);
                 
             }
-        })
+        });
       }
         runGame("addition");     
 
@@ -48,12 +49,43 @@ if(gameType === "addition") {
 
 }
 
-function checkAnswer(){
+/**
+ * checks the answer against the first element in
+ * the returned calculateCorrectAnswer
+ */
+ function checkAnswer() {
+
+  let userAnswer = parseInt(document.getElementById("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+
+  if (isCorrect) {
+      alert("Hey! You got it right! :D");
+  } else {
+      alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+  }
+
+  runGame(calculatedAnswer[1]);
 
 }
 
-function calculateCorrectAnwer(){
 
+/**
+ * Gets the opaerands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+
+function calculateCorrectAnwer(){
+          let operand1 = parseInt(document.getElementById('operand1').innerText);
+          let operand2 = parseInt(document.getElementById('operand2').innerText);
+          let operator = ducument.getElementById("operator").innerText;
+
+          if (operator ==="+") {
+            return[operand1 + operand2, "addition"]; 
+          }else {
+            alert(`unimplemented operator ${operator}`);
+            throw `unimplemented operator ${operator}.Aborting!`;
+          }
 }
 
 function incrementScore(){
