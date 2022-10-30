712ce54f3d960a7a445a86e5777f3ae9b8ffdc84
email2ify <email2ify@yahoo.com>
2022-10-30 09:36:38 +0000

    Add code for both multiplication and subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,12 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if(gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-} else {
+} else if (gameType ==="multiply"){
+  diplayMultiplyQuestion(num1, num2);
+} else if (gameType === "subtract"){
+  displaySubtractQuestion(num1, num2);
+}
+else {
      alert(`unknown game type: ${gameType}`);
      throw `unknown game type: ${gameType}.Aborting!`;
 }
@@ -122,10 +127,15 @@ function displayAdditionQuestion(operand1, operand2){
          document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion(){
-
+function displaySubtractQuestion(operand1, operand2){
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = "-";
 }
 
-function diplayMultiplyQuestion(){
 
+function diplayMultiplyQuestion(operand1,operand2){
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
 }
